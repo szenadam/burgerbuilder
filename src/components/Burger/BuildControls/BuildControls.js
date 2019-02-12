cd5668e5b1b2f156aee592a14ce2e15e8b600ae3
szenadam <szenadam1@gmail.com>
2019-02-12 14:05:42 +0100

    Add build control functionality

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -17,8 +17,11 @@ const buildControls = props => {
           key={ctrl.label}
           label={ctrl.label}
           added={() => props.ingredientAdded(ctrl.type)}
+          removed={() => props.ingredientRemoved(ctrl.type)}
+          disabled={props.disabled[ctrl.type]}
         />
       ))}
+      <div>{props.disabled[0]}</div>
     </div>
   );
 };
