2aae1a2cdb2d6c6b57ff749078aa75b475a74b23
szenadam <szenadam1@gmail.com>
2019-02-12 13:41:08 +0100

    Add build controls

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,9 +4,9 @@ import classes from './BuildControl.css';
 const buildControl = (props) => {
   return (
     <div className={classes.BuildControl}>
-      <div classNam={classes.Label}>{props.label}</div>
-      <button classNam={classes.Less}>Less</button>
-      <button classNam={classes.More}>More</button>
+      <div className={classes.Label}>{props.label}</div>
+      <button className={classes.Less}>Less</button>
+      <button className={classes.More}>More</button>
     </div>
   );
 };
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
 
-const buildControls = (props) => {
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Chees', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
+];
+
+const buildControls = props => {
   return (
-    <div>
-
+    <div className={classes.BuildControls}>
+      {controls.map(ctrl => (
+        <BuildControl key={ctrl.label} label={ctrl.label} />
+      ))}
     </div>
   );
 };
