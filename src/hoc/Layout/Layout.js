7139404147fdc4ecbba0864824f39f941bcf3ce4
szenadam <szenadam1@gmail.com>
2019-02-19 19:18:01 +0100

    Create logout navitem, add condition for menu auth navitem

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Aux from '../Auxiliary/Auxiliary';
 import classes from './Layout.css';
@@ -11,24 +12,37 @@ class Layout extends React.Component {
   };
 
   sideDrawerClosedHandler = () => {
-    this.setState({showSideDrawer: false});
-  }
+    this.setState({ showSideDrawer: false });
+  };
 
   sideDrawerToggleHandle = () => {
-    this.setState((prevState) => {
-      return {showSideDrawer: !prevState.showSideDrawer};
+    this.setState(prevState => {
+      return { showSideDrawer: !prevState.showSideDrawer };
     });
-  }
+  };
 
   render() {
     return (
       <Aux>
-        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandle} />
-        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+        <Toolbar
+          isAuth={this.props.isAuthenticated}
+          drawerToggleClicked={this.sideDrawerToggleHandle}
+        />
+        <SideDrawer
+          isAuth={this.props.isAuthenticated}
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
         <main className={classes.Content}>{this.props.children}</main>
       </Aux>
     );
   }
 }
 
-export default Layout;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
+
+export default connect(mapStateToProps)(Layout);
