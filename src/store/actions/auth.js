a4951b6eb707adcce39ca6d6efafffd5e125d062
szenadam <szenadam1@gmail.com>
2019-02-22 11:54:21 +0100

    Create authCheckStateSaga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -30,9 +28,6 @@ export const logoutSucceed = () => {
 };
 
 export const logout = () => {
-  // localStorage.removeItem('token');
-  // localStorage.removeItem('expirationDate');
-  // localStorage.removeItem('userId');
   return {
     type: actionTypes.AUTH_INITIATE_LOGOUT
   };
@@ -62,19 +57,7 @@ export const setAuthRedirectPath = path => {
 };
 
 export const authCheckState = () => {
-  return dispatch => {
-    const token = localStorage.getItem('token');
-    if (!token) {
-      dispatch(logout);
-    } else {
-      const expirationDate = new Date(localStorage.getItem('expirationDate'));
-      if (expirationDate <= new Date()) {
-        dispatch(logout());
-      } else {
-        const userId = localStorage.getItem('userId');
-        dispatch(authSuccess(token, userId));
-        dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));
-      }
-    }
-  };
+  return {
+    type: actionTypes.AUTH_CHECK_STATE
+  }
 };
--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -44,3 +44,19 @@ export function* authUserSaga(action) {
     yield put(actions.authFail(error.response.data.error)); // TODO Map error messages to proper sentences.
   }
 }
+
+export function* authCheckStateSaga(action) {
+  const token = yield localStorage.getItem('token');
+  if (!token) {
+    yield put(actions.logout());
+  } else {
+    const expirationDate = yield new Date(localStorage.getItem('expirationDate'));
+    if (expirationDate <= new Date()) {
+      yield put(actions.logout());
+    } else {
+      const userId = yield localStorage.getItem('userId');
+      yield put(actions.authSuccess(token, userId));
+      yield put(actions.checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));
+    }
+  }
+}
