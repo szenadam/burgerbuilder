c1ed2a3b7b9595f1352e399e858132087a44d963
szenadam <szenadam1@gmail.com>
2019-02-22 11:20:56 +0100

    Use logout saga for logging out

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
+import createSagaMiddleWare from 'redux-saga';
 
 import './index.css';
 import App from './App';
@@ -11,7 +12,12 @@ import * as serviceWorker from './serviceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
+import { watchAuth } from './store/sagas/index';
 
+const composeEnhancers =
+  process.env.NODE_ENV === 'development'
+    ? (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose)
+    : null;
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
@@ -19,7 +25,11 @@ const rootReducer = combineReducers({
   auth: authReducer
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const sagaMiddleware = createSagaMiddleWare();
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, sagaMiddleware)));
+
+sagaMiddleware.run(watchAuth);
 
 const app = (
   <Provider store={store}>
