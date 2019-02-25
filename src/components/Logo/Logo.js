46ec5ea27282da39ac2329a0041443b6e31e7d6e
szenadam <szenadam1@gmail.com>
2019-02-25 12:02:44 +0100

    Add link to logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.css';
 
-const logo = (props) => (
-  <div className={classes.Logo} style={{height: props.height}}>
-    <img src={burgerLogo} alt="MyBurger" />
+const logo = props => (
+  <div className={classes.Logo} style={{ height: props.height }}>
+    <Link to="/">
+      <img src={burgerLogo} alt="MyBurger" />
+    </Link>
   </div>
 );
 
